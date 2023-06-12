160adcd8eb111590b65c386e1fdb2d1bdc4728b1
Kumpel <bleidd04@gmail.com>
2023-06-12 18:55:10 +0200

    -(hopefully) adds permission handling

--- app/src/main/java/com/kumpello/whereiseveryone/ui/main/MainActivity.kt
+++ app/src/main/java/com/kumpello/whereiseveryone/ui/main/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.kumpello.whereiseveryone.ui.main
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,11 +16,46 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.content.ContextCompat
 import com.kumpello.whereiseveryone.ui.theme.WhereIsEveryoneTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var viewModel: MainActivityViewModel
+    lateinit var activity: MainActivity
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+    private var isBackGroundPermissionGranted = false
+    private var isFineLocationPermissionGranted = false
+    private var isCoarseLocationPermissionGranted = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val viewModel: MainActivityViewModel by viewModels()
+        this.viewModel = viewModel
+        activity = this
+        permissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    isBackGroundPermissionGranted =
+                        permissions[Manifest.permission.ACCESS_BACKGROUND_LOCATION]
+                            ?: isBackGroundPermissionGranted
+                }
+                isFineLocationPermissionGranted =
+                    permissions[Manifest.permission.ACCESS_FINE_LOCATION]
+                        ?: isFineLocationPermissionGranted
+                isCoarseLocationPermissionGranted =
+                    permissions[Manifest.permission.ACCESS_COARSE_LOCATION]
+                        ?: isCoarseLocationPermissionGranted
+                if (!isBackGroundPermissionGranted ||
+                        !isCoarseLocationPermissionGranted ||
+                        !isFineLocationPermissionGranted) {
+                        //TODO: Action when user deny permissions
+                }
+            }
+        requestPermissions()
+
         setContent {
             WhereIsEveryoneTheme {
                 // A surface container using the 'background' color from the theme
@@ -27,6 +68,44 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    private fun requestPermissions() {
+        isCoarseLocationPermissionGranted = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        isFineLocationPermissionGranted = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            isBackGroundPermissionGranted = ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_BACKGROUND_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+
+        val permissionRequestList = ArrayList<String>()
+
+        if (!isCoarseLocationPermissionGranted) {
+            permissionRequestList.add(Manifest.permission.READ_CONTACTS)
+        }
+
+        if (!isFineLocationPermissionGranted) {
+            permissionRequestList.add(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+
+        if (!isBackGroundPermissionGranted && Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            permissionRequestList.add(Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+        }
+
+        if (permissionRequestList.isNotEmpty()) {
+            permissionLauncher.launch(permissionRequestList.toTypedArray())
+        }
+    }
+
 }
 
 @Composable
