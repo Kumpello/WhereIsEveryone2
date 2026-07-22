0d12167de5c8b9a0c00ce74642661f7b51ca40d9
Kumpello <bleidd04@gmail.com>
2026-07-22 17:15:20 +0200

    *Accuracy correction

--- app/src/main/java/com/kumpello/whereiseveryone/main/common/entity/AccuracyLevel.kt
+++ app/src/main/java/com/kumpello/whereiseveryone/main/common/entity/AccuracyLevel.kt
@@ -1,8 +1,8 @@
 package com.kumpello.whereiseveryone.main.common.entity
 
 enum class AccuracyLevel(val haloSize: Double, val displayName: String) {
-    TRAGIC(6.0, "Tragic"),
-    LOW(4.0, "Low"),
+    TRAGIC(5.0, "Tragic"),
+    LOW(3.5, "Low"),
     MEDIUM(2.5, "Medium"),
     HIGH(1.0, "High"),
     PERFECT(0.0, "Perfect"),
